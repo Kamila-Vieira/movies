da33dd25541387ac751d4a6475e87a686e9425a8
Kamila-Vieira <vkamila54@outlook.com>
2022-04-24 22:15:38 -0300

    Ajuste paginação

--- src/hooks/usePaginate.tsx
+++ src/hooks/usePaginate.tsx
@@ -1,13 +1,8 @@
 import search from "../services/search";
 import { SEARCH_LIMIT_PER_PAGE } from "../mocks/defaults";
-import {
-  normalizeString,
-  separateArray,
-  sequentialArray,
-} from "../utils/Helpers";
+import { separateArray, sequentialArray } from "../utils/Helpers";
 import { useMoviesContext } from "../context/MoviesContext";
 import { Genre, MovieItem } from "../typings/movies";
-import { useEffect, useState } from "react";
 
 type Paginate = {
   activePage?: number;
@@ -16,91 +11,47 @@ type Paginate = {
 };
 
 const usePaginate = (updatePages: boolean) => {
-  const [newResults, setNewResults] = useState<MovieItem[]>([]);
   const { setState, state } = useMoviesContext();
-  const [totalPages, setTotalPages] = useState<number>(state.totalPages);
   const { page, initial, query, searchResult } = state;
-
-  useEffect(() => {
-    (async () => {
-      if (initial) {
-        const request = await search({
-          initial,
-          page: page + 1,
-        });
-        setNewResults(request?.results || []);
-        setTotalPages(request?.total_pages || 0);
-      } else {
-        const isValidQuery = query.replace(/[\s]/g, "").length;
-        const searchResults = await search(
-          isValidQuery > 0 ? { query, page } : { initial: true, page }
-        );
-
-        const searchResultsGenrer = await search({
-          initial: true,
-          page,
-        });
-
-        const genrerResultsFiltered =
-          searchResultsGenrer?.results?.filter((result) => {
-            const findGenre = state.genres.find(({ name }) => {
-              const normalizedName = normalizeString(name);
-              const normalizedQuery = normalizeString(query);
-              return normalizedName.includes(normalizedQuery);
-            });
-            return findGenre?.id && result.genre_ids.includes(findGenre?.id);
-          }) || [];
-
-        const matchYear = query.match(/[0-9]{4}/);
-
-        const searchResultsYear = matchYear
-          ? await search({ query: "a", page, year: matchYear[0] })
-          : { results: [], total_pages: 0 };
-        const searchResultsYearResults = searchResultsYear?.results || [];
-
-        const mapResults = new Set();
-
-        const filteredResults = [
-          ...(searchResults?.results || []),
-          ...genrerResultsFiltered,
-          ...searchResultsYearResults,
-        ].filter((result) => {
-          const duplicatedResult = mapResults.has(result.id);
-          mapResults.add(result.id);
-          return !duplicatedResult;
-        });
-        setNewResults(filteredResults);
-        setTotalPages(
-          (searchResults?.total_pages || 0) +
-            (searchResultsGenrer?.total_pages || 0) +
-            (searchResultsYear?.total_pages || 0)
-        );
-      }
-    })();
-
-    return () => {
-      setNewResults([]);
-    };
-  }, [initial]);
 
   return async ({
     activePage = state.activePage,
     oldSearchResult = searchResult,
     genres,
   }: Paginate) => {
+    if (initial) {
+    }
+    const newResults = await search(
+      initial
+        ? {
+            initial,
+            page: page + 1,
+          }
+        : {
+            query,
+            page: page + 1,
+          }
+    );
     const newPages = separateArray(
-      [...oldSearchResult, ...newResults],
+      [...oldSearchResult, ...(newResults?.results || [])],
       SEARCH_LIMIT_PER_PAGE
     );
-    const newSearchResults = [...oldSearchResult, ...newResults];
+    const newSearchResults = [
+      ...oldSearchResult,
+      ...(newResults?.results || []),
+    ];
+    const newMoviesResults = [
+      ...oldSearchResult,
+      ...(newResults?.results || []),
+    ];
 
     const newState = {
       activePage,
       searchResult: newSearchResults,
-      movies: newSearchResults,
+      movies: newMoviesResults,
       pages: newPages,
       searchLoading: false,
-      totalPages,
+      totalPages: newResults?.total_pages || state.totalPages,
       page: page + 1,
       limitPagination: newPages.length,
       updatedPages: updatePages
